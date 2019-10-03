d3c6ba0fc01432a0d4b05a4d342c7661c55de4d3
Anthony Yoskovich <anthonyyoskovich@Anthonys-MacBook-Pro.local>
2019-10-03 09:17:39 -0400

    Investigating prefixes

--- readDat.R
+++ readDat.R
@@ -7,31 +7,40 @@ rem_comm <- function(string){
   return(as.numeric(str_replace(string, '<1', '0')))
 }
 
-
 items <- read.csv('data/tacoItems.csv', stringsAsFactors = FALSE) %>%
   mutate_at(vars(-c('itemName', 'menuType')), rem_comm)
-
 
 prices <- read.csv('data/tacoPrices.csv', stringsAsFactors = FALSE) %>%
     mutate(price = as.numeric(str_remove(price, '\\$')))
 
-View(items %>%
-       inner_join(prices, by='itemName'), title='Matches')
+matches <- items %>%
+  inner_join(prices, by='itemName') %>%
+  select(itemName)
 
-View(items %>%
-  anti_join(prices, by='itemName'), title='Diffs')
 
-one <- items %>%
-       mutate(old = itemName) %>%
-       separate(itemName, c("A", "B"), sep='-') %>%
-       filter(!is.na(B))
 
-View(items %>%
-  filter(str_count(itemName, '-') == TRUE), title='All with -')
+dropSplit <- function(df, delim){
+  df %>%
+    mutate(old = itemName) %>%
+    separate(itemName, c('prefix','B'), sep=delim) %>%
+    filter(!is.na(B)) %>%
+    select(prefix) %>%
+    unique()
+}
+
+# 4 matches if we look at prefix
+prefMatches <- dropSplit(items, '-') %>%
+  inner_join(dropSplit(prices, '\\('), by='prefix')
+
+# Investigate those (pick up 20 obs)
+reg <- paste(prefMatches$prefix, collapse = '|')
+items %>%
+  filter(grepl(pattern=reg, itemName))
+
 
 # Cartesian product and get string distances (probs not the best way to do this)
-allSelf <- expand.grid(items$itemName, items$itemName) %>%
-  mutate(distance = stringdist(Var1, Var2))
+diff_items <- items %>% filter(!itemName %in% matches$itemName)
+diff_prices <- prices %>% filter(!itemName %in% matches$itemName)
 
-allBetween <- expand.grid(items$itemName, prices$itemName) %>%
+allBetween <- expand.grid(diff_items$itemName, diff_prices$itemName) %>%
   mutate(distance = stringdist(Var1, Var2))
