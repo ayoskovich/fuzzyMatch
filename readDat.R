369e9b085f212bd30eb20d658c6dcac7364f72d2
Anthony Yoskovich <anthonyyoskovich@Anthonys-MacBook-Pro.local>
2019-10-10 09:44:07 -0400

    Using existing package, not very good matching...

--- readDat.R
+++ readDat.R
@@ -2,45 +2,46 @@ require(dplyr)
 require(stringr)
 require(stringdist)
 require(tidyr)
+require(fuzzyjoin)
 
 rem_comm <- function(string){
   return(as.numeric(str_replace(string, '<1', '0')))
 }
 
 items <- read.csv('data/tacoItems.csv', stringsAsFactors = FALSE) %>%
-  mutate_at(vars(-c('itemName', 'menuType')), rem_comm)
+  mutate_at(vars(-c('itemName', 'menuType')), rem_comm) %>%
+  filter(!(menuType %in% c('cantina menu', 'cantina beer, wine, and spirits',
+                           'las vegas cantina', '16 oz fountain drinks',
+                           '20 oz fountain drinks', '30 oz fountain drinks',
+                           'hot sauce packets', 'freezies'))) %>%
+  select(itemName, menuType)
 
 prices <- read.csv('data/tacoPrices.csv', stringsAsFactors = FALSE) %>%
-    mutate(price = as.numeric(str_remove(price, '\\$')))
+  mutate(price = as.numeric(str_remove(price, '\\$'))) %>%
+  filter(!(genGroup %in% c('combo meals', 'drinks'))) %>%
+  select(itemName, genGroup)
 
-matches <- items %>%
-  inner_join(prices, by='itemName') %>%
-  select(itemName)
+# Using package, not very good
+View(items %>%
+  stringdist_left_join(prices, by='itemName'))
 
-
-
-dropSplit <- function(df, delim){
-  df %>%
-    mutate(old = itemName) %>%
-    separate(itemName, c('prefix','B'), sep=delim) %>%
-    filter(!is.na(B)) %>%
-    select(prefix) %>%
-    unique()
+# Remove matches
+rem_matches <- function(){
+  matches <- items %>%
+    inner_join(prices, by='itemName')
+  small_items <- items %>%
+    filter(!(itemName %in% matches$itemName))
+  small_prices <- prices %>%
+    filter(!(itemName %in% matches$itemName))
+  
+  # Cartesian product and get string distances
+  allBetween <- expand.grid(small_items$itemName, small_prices$itemName) %>%
+    mutate(distance = stringdist(Var1, Var2)) %>%
+    group_by(Var1) %>%
+    arrange(distance) %>%
+    filter(row_number(distance) == 1)
+  
+  return(allBetween)
 }
 
-# 4 matches if we look at prefix
-prefMatches <- dropSplit(items, '-') %>%
-  inner_join(dropSplit(prices, '\\('), by='prefix')
-
-# Investigate those (pick up 20 obs)
-reg <- paste(prefMatches$prefix, collapse = '|')
-items %>%
-  filter(grepl(pattern=reg, itemName))
-
-
-# Cartesian product and get string distances (probs not the best way to do this)
-diff_items <- items %>% filter(!itemName %in% matches$itemName)
-diff_prices <- prices %>% filter(!itemName %in% matches$itemName)
-
-allBetween <- expand.grid(diff_items$itemName, diff_prices$itemName) %>%
-  mutate(distance = stringdist(Var1, Var2))
+View(rem_matches())
